c7b727c43a56bdb4fe4fba5267c51531bf59546d
Emily Taysa de Souza Alves <cc25673@g.unicamp.br>
2026-05-21 10:25:04 -0300

    Update kanban2.js

--- kanban2.js
+++ kanban2.js
@@ -1,7 +1,13 @@
+let tarefas = [titulo = "", prioridade = "", status = ""];
 const form = document.getElementById("formtaf");
-
 
 function abrirFormulario() {
     form.style.display = "flex"
 }
+
+function fecharForm() {
+    form.style.display = "none"
+}
+
+
 
